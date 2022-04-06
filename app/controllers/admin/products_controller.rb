0021b9d01453f6b47e2d697bcea2ea5811815b1d
tofutigerz <hfu3@ualberta.ca>
2022-04-05 20:54:21 -0600

    adds basic http authentication for admin products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,40 +1,35 @@
 class Admin::ProductsController < ApplicationController
+	http_basic_authenticate_with name: ENV['ADMIN_USERNAME'],
+	                             password: ENV['ADMIN_PASSWORD']
+	def index
+		@products = Product.order(id: :desc).all
+	end
 
-  def index
-    @products = Product.order(id: :desc).all
-  end
+	def new
+		@product = Product.new
+	end
 
-  def new
-    @product = Product.new
-  end
+	def create
+		@product = Product.new(product_params)
 
-  def create
-    @product = Product.new(product_params)
+		if @product.save
+			redirect_to %i[admin products], notice: 'Product created!'
+		else
+			render :new
+		end
+	end
 
-    if @product.save
-      redirect_to [:admin, :products], notice: 'Product created!'
-    else
-      render :new
-    end
-  end
+	def destroy
+		@product = Product.find params[:id]
+		@product.destroy
+		redirect_to %i[admin products], notice: 'Product deleted!'
+	end
 
-  def destroy
-    @product = Product.find params[:id]
-    @product.destroy
-    redirect_to [:admin, :products], notice: 'Product deleted!'
-  end
+	private
 
-  private
-
-  def product_params
-    params.require(:product).permit(
-      :name,
-      :description,
-      :category_id,
-      :quantity,
-      :image,
-      :price
-    )
-  end
-
+	def product_params
+		params
+			.require(:product)
+			.permit(:name, :description, :category_id, :quantity, :image, :price)
+	end
 end
