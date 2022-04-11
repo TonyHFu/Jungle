a8e4995cd79077ea9da406cd54fdd254bcb42a2b
tofutigerz <hfu3@ualberta.ca>
2022-04-11 09:40:57 -0600

    implements tests to save product, validate name, and validate price

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+	it 'saves if no validation fails' do
+		@category = Category.new(name: 'pets')
+		@category.save!
+		@product =
+			Product.new(
+				name: 'dog',
+				description: "it's a dog",
+				category: @category,
+				quantity: 15,
+				price: 1500,
+			)
+		@product.save!
+		# p Product.all
+	end
+	it 'validates name is present' do
+		@category = Category.new(name: 'pets')
+		@category.save!
+		@product =
+			Product.new(
+				name: nil,
+				description: "it's a dog",
+				category: @category,
+				quantity: 15,
+				price: 1500,
+			)
+		@product.save
+		expect(@product.errors.full_messages).to include "Name can't be blank"
+	end
+	it 'validates price is present' do
+		@category = Category.new(name: 'pets')
+		@category.save!
+		@product =
+			Product.new(
+				name: 'doge',
+				description: "it's a dog",
+				category: @category,
+				quantity: 15,
+			)
+		@product.save
+		expect(@product.errors.full_messages).to include "Price can't be blank"
+	end
+	it 'validates quantity is present'
+	it 'validates category is present'
+end
