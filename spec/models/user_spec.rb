740da3e9c5300388d6bdf6abdaf04a1424476286
tofutigerz <hfu3@ualberta.ca>
2022-04-11 10:21:33 -0600

    implements check that password and password confirmation not blank

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-	it 'should have a password and password_confirmation' do
+	it 'should have a password' do
+		@user =
+			User.new(
+				first_name: 'bob',
+				last_name: 'builder',
+				email: 'bob@builder.com',
+				password: nil,
+				password_confirmation: nil,
+			)
+		@user.save
+		expect(@user.errors.full_messages).to include "Password can't be blank"
+	end
+	it 'should have a password_confirmation' do
 		@user =
 			User.new(
 				first_name: 'bob',
@@ -10,8 +22,10 @@ RSpec.describe User, type: :model do
 				password: '123456',
 				password_confirmation: nil,
 			)
-		@user.save!
-		p User.all
+		@user.save
+		expect(
+			@user.errors.full_messages,
+		).to include "Password confirmation can't be blank"
 	end
 	it 'should have a password and password_confirmation that match' do
 		@user =
@@ -22,7 +36,9 @@ RSpec.describe User, type: :model do
 				password: '123456',
 				password_confirmation: '456',
 			)
-		@user.save!
-		p User.all
+		@user.save
+		expect(
+			@user.errors.full_messages,
+		).to include "Password confirmation doesn't match Password"
 	end
 end
