0d4206666d7ee128802d98240a389b3b8313952e
tofutigerz <hfu3@ualberta.ca>
2022-04-12 16:58:18 -0600

    sends basic email on order placed

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,6 +8,8 @@ class OrdersController < ApplicationController
 		order = create_order(charge)
 
 		if order.valid?
+			@user = current_user
+			OrderMailer.order_confirmation(@user).deliver_later
 			empty_cart!
 			redirect_to order, notice: 'Your Order has been placed.'
 		else
--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,2 +1,8 @@
 class OrderMailer < ApplicationMailer
+	default from: 'reply@jungle.com'
+
+	def order_confirmation(user)
+		@user = user
+		mail(to: user.email, subject: 'Order Confirmation')
+	end
 end
