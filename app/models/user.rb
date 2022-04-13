d68313a8b8428f54eff9b854a9c3980aa914b395
tofutigerz <hfu3@ualberta.ca>
2022-04-12 18:38:45 -0600

    adds review model and table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
 	has_secure_password
+
+	has_many :review
+
 	validates :password, length: { minimum: 5 }
 	validates :password_confirmation, presence: true
 	validates :email, presence: true, uniqueness: { case_sensitive: false }
