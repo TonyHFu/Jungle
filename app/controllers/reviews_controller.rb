b69f06bd1e537c64c720f83880650805a36e0f10
tofutigerz <hfu3@ualberta.ca>
2022-04-12 20:14:44 -0600

    creates create method in reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,19 @@
 class ReviewsController < ApplicationController
+	def create
+		@product = Product.find params[:product_id]
+		@review = @product.review.new review_params
+		@review.user = current_user
+
+		if @review.save
+			redirect_to product_path(@product)
+		else
+			raise 'Error saving review'
+		end
+	end
+
+	private
+
+	def review_params
+		params.require(:review).permit(:rating, :body, :product_id)
+	end
 end
