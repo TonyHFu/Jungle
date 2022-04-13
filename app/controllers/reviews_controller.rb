f9cb6ea65f78d35c8a2d84c873f5362209acea91
tofutigerz <hfu3@ualberta.ca>
2022-04-12 20:48:03 -0600

    before_filter to hide review controller methods and allows delete own reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,6 @@
 class ReviewsController < ApplicationController
+	before_filter :require_login
+
 	def create
 		@product = Product.find params[:product_id]
 		@review = @product.review.new review_params
@@ -7,13 +9,24 @@ class ReviewsController < ApplicationController
 		if @review.save
 			redirect_to product_path(@product)
 		else
-			raise 'Error saving review'
+			redirect_to product_path(@product), notice: 'Error saving review'
 		end
+	end
+
+	def destroy
+		@review = Review.find params[:id]
+		@product = @review.product
+		@review.destroy
+		redirect_to product_path(@product), notice: 'Review deleted!'
 	end
 
 	private
 
 	def review_params
 		params.require(:review).permit(:rating, :body, :product_id)
+	end
+
+	def require_login
+		redirect_to login_path, notice: 'You must be logged in' unless current_user
 	end
 end
